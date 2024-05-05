1cc4bc824243f62f21f3afcdaabb05c055e1498d
avd151 <apurvadeshpande207@gmail.com>
2024-05-05 10:16:34 +0530

    add: templating and gmail support

--- src/main/java/com/emailsvc/backend/controller/EmailController.java
+++ src/main/java/com/emailsvc/backend/controller/EmailController.java
@@ -6,10 +6,16 @@ package com.emailsvc.backend.controller;
 import com.emailsvc.backend.model.NotificationRequest;
 import com.emailsvc.backend.service.NotificationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import java.util.List;
+import java.util.Map;
 
 //import com.empmgmt.backend.model.User;
 //import com.empmgmt.backend.repository.UserRepository;
@@ -37,23 +43,39 @@ public class EmailController {
     }
 
     @PostMapping("/apply")
-    public String apply(@RequestBody NotificationRequest notificationRequest){
+    public String apply(@RequestBody NotificationRequest notificationRequest) throws MessagingException {
         String channel = notificationRequest.channel;
-        String content = "empty";
 //		if(channel.equalsIgnoreCase("Email")){
 //			EmailNotification emailNotification = notificationRequest.notification.getEmailNotification();
 //
 //		}
-        content = notificationService.applyTemplate(notificationRequest);
+        String sender = notificationRequest.sender.trim();
+        String receiver = notificationRequest.receiver.trim();
+        String subject = notificationRequest.emailNotification.body.getOrDefault("subject",
+                "Demo subject");
+        String content = notificationService.applyTemplate(notificationRequest);
         System.out.println(notificationRequest.channel);
-        System.out.println(notificationRequest.sender);
-        System.out.println(notificationRequest.receiver);
-        System.out.println(notificationRequest.emailNotification);
+        System.out.println(sender);
+        System.out.println(receiver);
+        System.out.println(subject);
+        System.out.println(content);
+
+        notificationService.sendEmailDynamic(sender, receiver
+                , subject, content);
         return content;
     }
 
     @PostMapping("/email")
-    public String sendEmail(){
-        return null;
+//    @EventListener(ApplicationReadyEvent.class)
+    public String sendEmail(@RequestBody Map<String, List<String>> body){
+        List<String> allToEmails = body.get("emails");
+        String subject = body.get("subject").get(0);
+        String emailBody = body.get("body").get(0);
+//        String subject = "Looking for opportunities in ";
+        for(String toEmail: allToEmails){
+            notificationService.sendEmail(toEmail, subject,
+                    emailBody);
+        }
+        return "OK";
     }
 }
--- src/main/java/com/emailsvc/backend/service/NotificationServiceImpl.java
+++ src/main/java/com/emailsvc/backend/service/NotificationServiceImpl.java
@@ -2,11 +2,18 @@ package com.emailsvc.backend.service;
 
 import com.emailsvc.backend.model.EmailNotification;
 import com.emailsvc.backend.model.NotificationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.websocket.Session;
 import java.util.HashMap;
 import java.util.Locale;
@@ -15,10 +22,46 @@ import java.util.Properties;
 @Service
 public class NotificationServiceImpl {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     private final TemplateEngine templateEngine;
 
     public NotificationServiceImpl(TemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
+    }
+
+    public void sendEmail(String toEmail,
+                                String subject,
+                                String body
+    ) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("<your email>");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+        mailSender.send(message);
+        System.out.printf("Mail Sent to %s\n", toEmail);
+    }
+
+    public void sendEmailDynamic(String fromEmail, String toEmail,
+                          String subject,
+                          String body
+    ) throws MessagingException {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom(fromEmail);
+//        message.setTo(toEmail);
+//        message.setText(body);
+//        message.setSubject(subject);
+//        mailSender.send(message);
+        MimeMessage mimeMailMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMailMessage, true, "UTF-8");
+        mimeMailMessage.setFrom(fromEmail);
+        helper.setTo(toEmail);
+        helper.setText(body, true);
+        helper.setSubject(subject);
+        mailSender.send(mimeMailMessage);
+        System.out.printf("Mail Sent to %s\n", toEmail);
     }
 
     public String applyTemplate(NotificationRequest notificationRequest){
@@ -39,7 +82,16 @@ public class NotificationServiceImpl {
         HashMap<String, String> contextVarsMp = emailNotification.body;
         if(contextVarsMp == null)
             return "body empty";
-        Context context = new Context();
+        String lang = notificationRequest.lang;
+        if(lang.isEmpty() || lang == null)
+            return "language empty";
+        Locale locale;
+//        if(lang.equalsIgnoreCase("english"))
+//            locale = (Locale.ENGLISH);
+//        else
+//            locale = (Locale.FRENCH);
+        locale = Locale.forLanguageTag(lang);
+        Context context = new Context(locale);
         for(String key : contextVarsMp.keySet()){
             context.setVariable(key, contextVarsMp.get(key));
         }
@@ -47,26 +99,26 @@ public class NotificationServiceImpl {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasenames("i18n");
         messageSource.setDefaultEncoding("UTF-8");
-        System.out.println(messageSource.getMessage("greeting", null, Locale.ENGLISH));
+        System.out.println(messageSource.getMessage("greeting", null, locale));
 //        SpringApplication.run(Javai18nspringbootApplication.class, args);
-        String templateFile = "i18n.html";
+        String templateFile = "email1.html";
         content = templateEngine.process(templateFile, context);
         return content;
     }
 
-    public String sendEmail(String sender, String subject, String content){
-        String host = "smtp.gmail.com";
-        Properties properties = System.getProperties();
-        System.out.println(properties);
-
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", 465);
-        properties.put("mail.smtp.ssl.enable", "true");
-        properties.put("mail.smtp.auth", "true");
-
-        //get session object
-
-        return null;
-    }
+//    public String sendEmail(String sender, String subject, String content){
+//        String host = "smtp.gmail.com";
+//        Properties properties = System.getProperties();
+//        System.out.println(properties);
+//
+//        properties.put("mail.smtp.host", host);
+//        properties.put("mail.smtp.port", 465);
+//        properties.put("mail.smtp.ssl.enable", "true");
+//        properties.put("mail.smtp.auth", "true");
+//
+//        //get session object
+//
+//        return null;
+//    }
 
 }
